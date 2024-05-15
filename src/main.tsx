197411bdbd507bba7024a2a1808ca4403809229a
Wei Zhang <cyoi0129@gmail.com>
2024-05-15 11:06:33 +0900

    Add service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { UpdateDialog } from "./components";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
 );
+
+serviceWorkerRegistration.register({
+  onUpdate: registration => {
+    if (registration.waiting) {
+      ReactDOM.render(<UpdateDialog registration={registration} />, 
+        document.querySelector('.SW-update-dialog'));
+    }
+  }
+});
