7792e669e0643d2c791056ec5faade5fdb592834
cyoi0129 <cyoi0129@gmail.com>
2024-11-29 20:55:24 +0900

    add ga

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from "./app/store";
 import App from './App';
+import ReactGA from "react-ga4";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+ReactGA.initialize("G-9PYMVNMC7M");
+ReactGA.send("pageview");
 
 root.render(
   <Provider store={store}>
